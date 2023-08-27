a4b497a562be7b5601a491e4cfcf8ebd7cbd72fb
MUNEERWANI <muneerwani78@gmail.com>
2023-08-27 19:14:35 +0530

    Geeting started with   MeetUp  app

---
+++ app/news/something.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Important = () => {
+  return (
+    <div>
+      <h1>So mention filee name if you dont use page.js</h1>
+    </div>
+  )
+}
+
+export default Important
